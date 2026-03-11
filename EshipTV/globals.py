3d826d53ff63b49abe69bb82991cfcd7a74ad05e
rasp-pi3 <rasppi3@gmail.com>
2026-03-11 12:38:33 -0300

    update

---
+++ EshipTV/globals.py
@@ -0,0 +1,10 @@
+URL = "https://www.eship.com.br"
+URL_LOGGED = "https://www.eship.com.br/?HOME"
+
+HTML_ID_LOGIN = "FORMULARIO_LOGAR_login"
+HTML_ID_PASSWORD = "FORMULARIO_LOGAR_senha"
+HTML_ID_BUTTON = "FORMULARIO_LOGAR_Entrar"
+
+JSON_FILE = "user.json"
+
+user = dict(user="", password="", url=URL, timeout=10.0)
---
+++ inicio.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd EshipTV
+sudo python3 main.py
+read
